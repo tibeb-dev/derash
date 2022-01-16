dd9c940e80a770f086f4d083595cbb7b47d2ba1e
degeman <96534290+degeman@users.noreply.github.com>
2022-01-17 00:14:49 +0300

    Add files via upload

---
+++ src/main/java/com/webproject/derash/DerashApplication.java
@@ -0,0 +1,13 @@
+package com.webproject.derash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DerashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DerashApplication.class, args);
+	}
+
+}
