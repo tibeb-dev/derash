0bd460e3ab70fc92a5c5894cde1af50b0d2f0554
seb <seblina1224@gmail.com>
2022-01-17 15:47:37 +0300

    add home page

---
+++ derash-master/src/test/java/com/webproject/derash/DerashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webproject.derash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DerashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
