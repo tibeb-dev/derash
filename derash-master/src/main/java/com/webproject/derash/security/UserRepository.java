e9f39fd6fa91bfc42cf6d15833881d3f5c4f9b34
degeman <96534290+degeman@users.noreply.github.com>
2022-01-22 03:11:30 -0800

    Update UserRepository.java

--- derash-master/src/main/java/com/webproject/derash/security/UserRepository.java
+++ derash-master/src/main/java/com/webproject/derash/security/UserRepository.java
@@ -5,5 +5,6 @@ import org.springframework.data.repository.CrudRepository;
 public interface UserRepository 
 extends CrudRepository<User, Long> {
    public User findByUsername(String username);
+   public User findByEmail(String email);
 }
 
