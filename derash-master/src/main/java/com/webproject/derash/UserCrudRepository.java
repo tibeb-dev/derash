62677eb3b465e234956d977977700b8080be154a
degeman <96534290+degeman@users.noreply.github.com>
2022-01-22 08:08:31 -0800

    Delete UserCrudRepository.java

--- derash-master/src/main/java/com/webproject/derash/UserCrudRepository.java
+++
@@ -1,8 +0,0 @@
-package com.webproject.derash;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserCrudRepository extends CrudRepository<RegisterPage, String> {
-
-	public RegisterPage findByEmail(String email);
-}
