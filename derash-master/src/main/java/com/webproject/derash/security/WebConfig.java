8978f30c9be0298600720558bd419037ef1a42cf
degeman <96534290+degeman@users.noreply.github.com>
2022-01-22 12:27:25 -0800

    added about page to view controller

--- derash-master/src/main/java/com/webproject/derash/security/WebConfig.java
+++ derash-master/src/main/java/com/webproject/derash/security/WebConfig.java
@@ -11,6 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
    public void addViewControllers(ViewControllerRegistry registry) {
        registry.addViewController("/").setViewName("home");
        registry.addViewController("/login").setViewName("login");
+       registry.addViewController("/about").setViewName("about");
    }
 
 }
